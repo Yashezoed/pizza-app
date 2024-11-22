773e8054e7af5091e1676918dad1b46e2deffb31
Yashen <mr.efyx@mail.ru>
2024-11-22 22:26:08 +0300

    12.9

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Cart } from './pages/Cart/Cart.tsx';
-import { Menu } from './pages/Menu/Menu.tsx';
-import { Error } from './pages/Error/Error.tsx';
+import { Error as ErrorPage } from './pages/Error/Error.tsx';
 import { Layout } from './layout/Layout/Layout.tsx';
 import { Product } from './pages/Product/Product.tsx';
+import axios from 'axios';
+import { PREFIX } from './helpers/api.ts';
+
+const Menu = lazy(() => import('./pages/Menu//Menu.tsx'));
 
 const router = createBrowserRouter(
 	[
@@ -16,7 +19,11 @@ const router = createBrowserRouter(
 			children: [
 				{
 					path: '/',
-					element: <Menu />
+					element: (
+						<Suspense fallback={<>Загрузка...</>}>
+							<Menu />
+						</Suspense>
+					)
 				},
 				{
 					path: '/cart',
@@ -24,7 +31,19 @@ const router = createBrowserRouter(
 				},
 				{
 					path: '/product/:id',
-					element: <Product />
+					element: <Product />,
+					errorElement: <>Ошибка</>,
+					loader: async ({ params }) => {
+						await new Promise<void>((resolve) => {
+							setTimeout(() => {
+								resolve();
+							}, 2000);
+						});
+						const { data } = await axios.get(
+							`${PREFIX}/products/${params.id}`
+						);
+						return data;
+					}
 				}
 			]
 		},
@@ -34,7 +53,7 @@ const router = createBrowserRouter(
 		},
 		{
 			path: '*',
-			element: <Error />
+			element: <ErrorPage />
 		}
 	],
 	{
